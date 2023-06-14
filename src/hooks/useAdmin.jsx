52f7fe0b4c3a8c94e087c71b799b0d60669832b7
IamLikhon1 <you@example.com>
2023-06-14 12:15:33 +0600

    fix name

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -3,11 +3,13 @@ import useAuth from "./useAuth"
 import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin=()=>{
+
     const{user,loading}=useAuth();
     const [axiosSecure]=useAxiosSecure();
     const{data:isAdmin, isLoading: isAdminLoading}=useQuery({
         queryKey:['isAdmin',user?.email],
         enabled:!loading,
+        
         queryFn:async()=>{
             const res= await axiosSecure.get(`/users/admin/${user?.email}`);        
             return res.data.admin
